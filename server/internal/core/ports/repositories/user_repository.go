1a84e8cd9e87be746a37803a46a99bbc1239cee7
lc-nixOS <lcommander101010@gmail.com>
2025-06-11 18:44:48 -0500

    refactor: remove user management feature and related code
    This commit removes the entire user management feature including:
    - Domain entities and repositories
    - HTTP and gRPC adapters
    - Database models and connection
    - Service layer and utilities
    - All related test files and dependencies

--- server/internal/core/ports/repositories/user_repository.go
+++
@@ -1,8 +0,0 @@
-package repositories
-
-import "github.com/t-saturn/central-user-manager/server/internal/core/domain/entities"
-
-type UserRepository interface {
-	Create(user *entities.User) error
-	// Puedes agregar más métodos luego: GetByID, GetAll, etc.
-}
