1b51f242077c69178b98e275a34821a6ab9c5d2a
lc-nixOS <lcommander101010@gmail.com>
2025-06-12 06:05:23 -0500

    feat(user): implement user creation service with gRPC
    Add proto definition for User service with CreateUser RPC
    Implement model, repository, service and gRPC handler layers
    Add Makefile for proto compilation and server commands
    Update dependencies for UUID and protobuf support

--- server/cmd/grpc/main.go
+++ server/cmd/grpc/main.go
@@ -4,26 +4,45 @@ import (
 	"fmt"
 	"net"
 
+	"google.golang.org/grpc"
+
+	"github.com/t-saturn/central-user-manager/server/internal/models"
+	userpb "github.com/t-saturn/central-user-manager/server/pb/proto"
 	"github.com/t-saturn/central-user-manager/server/pkg/config"
 	"github.com/t-saturn/central-user-manager/server/pkg/database"
 	"github.com/t-saturn/central-user-manager/server/pkg/logger"
-	"google.golang.org/grpc"
+
+	handler "github.com/t-saturn/central-user-manager/server/internal/grpc"
+	"github.com/t-saturn/central-user-manager/server/internal/repositories"
+	"github.com/t-saturn/central-user-manager/server/internal/services"
 )
 
 func main() {
-	cfg := config.Load()
-
+	// Inicializar logger
 	logger.InitLogger()
 
+	// Cargar configuración
+	cfg := config.Load()
+
+	// Inicializar base de datos
 	database.Init(cfg)
 
+	// Migrar automáticamente el modelo User
+	_ = database.DB.AutoMigrate(&models.User{})
+
+	// Iniciar listener gRPC
 	addr := fmt.Sprintf(":%s", cfg.GRPCPort)
 	lis, err := net.Listen("tcp", addr)
 	if err != nil {
 		logger.Log.Fatalf("Failed to start listener in %s: %v", addr, err)
 	}
 
+	// Crear servidor gRPC
 	s := grpc.NewServer()
+
+	repo := repositories.NewUserRepository()
+	svc := services.NewUserService(repo)
+	userpb.RegisterUserServiceServer(s, handler.NewUserHandler(svc))
 
 	logger.Log.Infof("User management gRPC server started in %s", addr)
 
--- server/go.mod
+++ server/go.mod
@@ -3,9 +3,11 @@ module github.com/t-saturn/central-user-manager/server
 go 1.24.4
 
 require (
+	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
 	github.com/sirupsen/logrus v1.9.3
 	google.golang.org/grpc v1.73.0
+	google.golang.org/protobuf v1.36.6
 	gorm.io/driver/postgres v1.6.0
 	gorm.io/gorm v1.30.0
 )
@@ -23,5 +25,4 @@ require (
 	golang.org/x/sys v0.31.0 // indirect
 	golang.org/x/text v0.23.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250324211829-b45e905df463 // indirect
-	google.golang.org/protobuf v1.36.6 // indirect
 )
