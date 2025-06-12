59326359f0c69e742b482a7673c1bc5af99d0909
lc-nixOS <lcommander101010@gmail.com>
2025-06-12 06:26:16 -0500

    refactor(grpc): move grpc server setup to internal package
    The gRPC server setup logic was moved from main.go to the internal/grpc package to improve code organization and maintainability. This includes the server initialization, service registration, and database setup.

---
+++ server/internal/grpc/server.go
@@ -0,0 +1,35 @@
+package grpc
+
+import (
+	"fmt"
+	"net"
+
+	"google.golang.org/grpc"
+
+	"github.com/t-saturn/central-user-manager/server/pkg/config"
+	"github.com/t-saturn/central-user-manager/server/pkg/database"
+	"github.com/t-saturn/central-user-manager/server/pkg/logger"
+
+	"github.com/t-saturn/central-user-manager/server/internal/models"
+)
+
+func StartGRPCServer(cfg *config.Config) {
+	database.Init(cfg)
+	_ = database.DB.AutoMigrate(&models.User{})
+
+	addr := fmt.Sprintf(":%s", cfg.GRPCPort)
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		logger.Log.Fatalf("Failed to start listener in %s: %v", addr, err)
+	}
+
+	s := grpc.NewServer()
+
+	RegisterServices(s)
+
+	logger.Log.Infof("User management gRPC server started in %s", addr)
+
+	if err := s.Serve(lis); err != nil {
+		logger.Log.Fatalf("Failed to serve gRPC: %v", err)
+	}
+}
