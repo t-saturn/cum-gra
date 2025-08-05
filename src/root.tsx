867bb1f4b7b4d4f881e4ee524f7663a1290361f2
rahman <nix.os.lc@gmail.com>
2025-08-05 16:26:29 -0500

    Remove unused components and styles for a cleaner codebase, including gauge, header, hero, infobox, next-steps, and demo routes. Simplify layout structure by eliminating unnecessary elements and styles.

--- src/root.tsx
+++ src/root.tsx
@@ -1,7 +1,6 @@
 import { component$ } from "@builder.io/qwik";
 import { isDev } from "@builder.io/qwik";
 import { QwikCityProvider, RouterOutlet } from "@builder.io/qwik-city";
-import { RouterHead } from "./components/router-head/router-head";
 
 import "./global.css";
 
@@ -23,7 +22,6 @@ export default component$(() => {
             href={`${import.meta.env.BASE_URL}manifest.json`}
           />
         )}
-        <RouterHead />
       </head>
       <body lang="en">
         <RouterOutlet />
