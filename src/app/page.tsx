c7829e8b19c68c35b9c4284ca5b291be258b982e
rahman <nix.os.lc@gmail.com>
2025-08-07 08:53:15 -0500

    feat: add ToastTest component and Toaster UI integration with toast notifications

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
+'use client';
+
+import ToastTest from '@/components/test/toast-test';
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       SOO FRONTEND
+      <div>
+        <ToastTest />
+      </div>
     </div>
   );
 }
