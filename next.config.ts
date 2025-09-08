a077b7efeaf5b9de613f7209c84681260a07742d
rahman <nix.os.lc@gmail.com>
2025-09-08 12:01:36 -0500

    fix: update image configuration to use remote patterns for enhanced security

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,18 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['raw.githubusercontent.com', 'cdn.jsdelivr.net'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        pathname: '/t-saturn/resources/**', // <-- más seguro, solo tu repo
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.jsdelivr.net',
+      },
+    ],
   },
-  /* config options here */
 };
 
 export default nextConfig;
